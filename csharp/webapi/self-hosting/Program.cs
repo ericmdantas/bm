1e6d4af361da563641e1460d0d65085fedead51e
Bernardo Esbérard <bernardo@n4works.com>
2016-04-01 17:04:57 -0300

    Adding webapi.owin.self-hosting

---
+++ csharp/webapi/self-hosting/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WebApi.Owin.SelfHosting
+{
+    using System;
+
+    using Microsoft.Owin.Hosting;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            const string ADDRESS = "http://localhost:3000/";
+
+            using (WebApp.Start<Startup>(ADDRESS))
+            {
+                Console.WriteLine("Your application is running on " + ADDRESS);
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ csharp/webapi/self-hosting/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApi.Owin.SelfHosting
+{
+    using System.Web.Http;
+
+    using global::Owin;
+
+    /// <summary>
+    /// The startup class.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="appBuilder">
+        /// The app builder.
+        /// </param>
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("DefaultApi", "{controller}");
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
